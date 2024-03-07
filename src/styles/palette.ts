b95a06ef441e78c039e07e9097d76bf07472bdc1
Abdul Rahman <arkniazi.dev@gmail.com>
2024-03-07 16:23:45 +0500

    ColorTheming: Set palette for colors.

---
+++ src/styles/palette.ts
@@ -0,0 +1,29 @@
+export const palette = {
+  white: "#ffffff",
+  black: "#000000",
+  purple: {
+    purple500: "#7822ff",
+    purple600: "#5605c0"
+  },
+  grey: {
+    grey75: "#bfbfbf",
+    grey200: "#e3e9ef",
+    grey700: "#414d5a",
+    grey900: "#10181b"
+  },
+  red: {
+    red100: "#ffe6e6",
+    red500: "#fe4646"
+  },
+  blue: {
+    blue100: "#cce5ff",
+    blue200: "#aed4fc",
+    blue500: "#0060b9"
+  },
+  navy: {
+    navy500: "#133774"
+  },
+  darkBlue: {
+    darkBlue500: "#0d2e65"
+  }
+} as const;
