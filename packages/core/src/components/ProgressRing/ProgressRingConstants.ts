f0008c06c3eca95ba0e0817144763431b26a9184
Abdul Rahman <arkniazi.dev@gmail.com>
2024-04-18 20:44:33 +0500

    ProgressRing: Create progress ring component and design accordingly.

---
+++ packages/core/src/components/ProgressRing/ProgressRingConstants.ts
@@ -0,0 +1,13 @@
+export enum ProgressRingColors {
+    PRIMARY = "primary",
+    SUCCESS = "success",
+    ERROR = "error"
+};
+
+export enum ProgressRingSizes {
+    SMALL = "small",
+    MEDIUM = "medium",
+    LARGE = "large",
+    XL = "xl",
+    XXL = "xxL"
+};
